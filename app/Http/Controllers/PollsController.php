a1011d4a432279f75b9572af3a16e06a6f5741f4
Saleh Rezq <salehrezq@gmail.com>
2018-12-09 12:18:29 +0200

    API Browse => get all polls

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Poll;
 use Illuminate\Http\Request;
 
 class PollsController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Poll::get(), 200);
+    }
+
 }
