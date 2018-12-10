9eae3e13b3646a15fa454812606a853bf39101b2
Saleh Rezq <salehrezq@gmail.com>
2018-12-10 15:14:39 +0200

    API Add => post a poll

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -7,14 +7,29 @@ use Illuminate\Http\Request;
 
 class PollsController extends Controller
 {
+    // http://localhost:8000/api/polls
     public function index()
     {
         return response()->json(Poll::get(), 200);
     }
 
+    // http://localhost:8000/api/polls/1
     public function show($id)
     {
         return response()->json(Poll::find($id), 200);
+    }
+
+    /**
+     * http://localhost:8000/api/polls
+     *
+     * Pass the data to this method as a form data or as json text.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store()
+    {
+        $poll = Poll::create(request()->all());
+        return response()->json($poll, 201);
     }
 
 }
