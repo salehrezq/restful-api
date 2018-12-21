294ee07b082b075e4b1bc9c682c616e65f68cc87
Saleh Rezq <salehrezq@gmail.com>
2018-12-21 16:43:20 +0200

    Add logging middleware
    You can find logs at this path: storage/logs/laravel.log

--- app/Http/Middleware/Logging.php
+++ app/Http/Middleware/Logging.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class Logging
 {
@@ -15,6 +16,20 @@ class Logging
      */
     public function handle($request, Closure $next)
     {
+        // You can find logs at this path: storage/logs/laravel.log
+        Log::debug($request->method());
         return $next($request);
+    }
+
+    /**
+     * Handle an outgoing response.
+     *
+     * @param $request
+     * @param $response
+     */
+    public function terminate($request, $response)
+    {
+        // You can find logs at this path: storage/logs/laravel.log
+        Log::debug($response->status());
     }
 }
