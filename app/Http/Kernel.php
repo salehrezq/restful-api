14aa5ce62c77109abf32c14c1a641b91b83244e9
Saleh Rezq <salehrezq@gmail.com>
2018-12-22 00:12:20 +0200

    Add token authentication
    Basic example of token authentication via headers.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,8 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\Logging::class,
-            'httpHeaders:Come Work For US!'
+            'httpHeaders:Come Work For US!',
+            \App\Http\Middleware\TokenAuth::class
         ],
     ];
 
--- app/Http/Middleware/TokenAuth.php
+++ app/Http/Middleware/TokenAuth.php
@@ -15,6 +15,19 @@ class TokenAuth
      */
     public function handle($request, Closure $next)
     {
+        $token = $request->header('X-API-TOKEN');
+
+        /**
+         * In Postman, test this middleware by setting a key-value pair
+         * in the Headers section of the request (any request):
+         *
+         * key: X-API-TOKEN
+         * value: test-value
+         *
+         */
+        if ('test-value' !== $token) {
+            abort(401, 'Auth Token not found');
+        }
         return $next($request);
     }
 }
